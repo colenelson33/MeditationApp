0aa343a0a9e099aa60896932d109e25799bb7caf
Cole Nelson <colenelson1210@gmail.com>
2023-02-25 17:06:12 -0500

    Moved images to folder

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ Array(3000).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('./space.jpeg');
+const spaceTexture = new THREE.TextureLoader().load('./images/space.jpeg');
 scene.background = spaceTexture;
 
 // Avatar
@@ -73,7 +73,7 @@ scene.background = spaceTexture;
 
 // Moon
 
-const sunTexture = new THREE.TextureLoader().load('./sun.jpeg');
+const sunTexture = new THREE.TextureLoader().load('./images/sun.jpeg');
 const sun = new THREE.Mesh(
   new THREE.SphereGeometry(25, 32, 32),
   new THREE.MeshStandardMaterial({
@@ -85,14 +85,14 @@ sun.position.z = -550
 sun.position.setX(200);
 
 
-const moonTexture = new THREE.TextureLoader().load('./moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('./normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('./images/moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('./images/normal.jpg');
 
 
 
 
-const earthTexture = new THREE.TextureLoader().load('./earth.jpeg');
-const normalEarthTexture = new THREE.TextureLoader().load('./earthNormal.jpg');
+const earthTexture = new THREE.TextureLoader().load('./images/earth.jpeg');
+const normalEarthTexture = new THREE.TextureLoader().load('./images/earthNormal.jpg');
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(7, 32, 32),
   new THREE.MeshStandardMaterial({
